3a6904016d68cd75c0af72802ac6e6da6f0e8969
Adarsh <adarshsanjeev@gmail.com>
2015-12-06 20:49:49 +0500

    Bunch of bug fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# CalenZone
+ # CalenZone
 
 CalenZone is am event tracker and manager created using Web2Py and JavaScript.
 
-# Installation
+ # Installation
 
 Git clone the repository into the applications folder of Web2Py and start web2py.
--- models/menu.py
+++ models/menu.py
@@ -12,8 +12,8 @@ response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
+response.meta.author = 'CalenZone'
+response.meta.description = 'A really cool event tracker'
 response.meta.keywords = 'web2py, python, framework'
 response.meta.generator = 'Web2py Web Framework'
 
@@ -135,4 +135,4 @@ def _():
         ]
 if DEVELOPMENT_MENU: _()
 
-if "auth" in locals(): auth.wikimenu() 
+if "auth" in locals(): auth.wikimenu()
