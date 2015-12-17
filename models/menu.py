c9a1d8de3a7b24efd0cb931891230cc17773b71c
Rishith Reddy <babarishith@gmail.com>
2015-12-17 23:18:02 +0530

    my events

--- models/menu.py
+++ models/menu.py
@@ -25,7 +25,7 @@ response.google_analytics_id = None
 response.menu = [
     (T('Home'), False, URL('default', 'index'), []),
     (T('Create Event'), False, URL('default', 'createEvent'), []),
-    (T('My Events'), False, URL('default', 'setEventTags'), []),
+    (T('My Events'), False, URL('default', 'myEvents'), []),
     (T('My Groups'), False, URL('default', 'profile'), []),
 ]
 
