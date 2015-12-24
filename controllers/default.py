0fcb5fc075930513f634907824ceea12efc5f90e
mukul hase <mukulhase@gmail.com>
2015-12-24 20:59:19 +0530

    Column Visibility Toggle, Calendar UI Change, More Details on search page

--- controllers/default.py
+++ controllers/default.py
@@ -30,7 +30,7 @@ def search():
     q1 = db.userTag.tag == db.eventTag.tag
     q1 &= (db.userTag.auth_user == session.auth.user.id)
     q1 &= (db.events.id == db.eventTag.events)
-    res = db(q1).select(db.events.eventName, db.events.startAt, db.events.endAt)
+    res = db(q1).select(db.events.ALL)
     return dict(res=res, key=key)
 
 def iCal():
